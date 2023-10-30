5ac80693a69c20eb07085164b9d0f265d3ed97e3
abhishek nair <abhisheknair@gmail.com>
2023-10-30 10:19:07 +0530

    committed 10/30/2023, 10:19:06 AM

--- set03/problem01.c
+++ set03/problem01.c
@@ -14,7 +14,7 @@ int main(){
 void input(float *x1, float *y1, float *x2, float *y2){
     printf("x1: ");
     scanf("%f",x1);
-    printf("%d", )
+    printf("%d", x1);
     printf("y1: ");
     scanf("%f",y1);
     printf("x2: ");
@@ -25,7 +25,7 @@ void input(float *x1, float *y1, float *x2, float *y2){
 float find_distance(float x1, float y1, float x2, float y2){
     float sq1 = (x2 - x1)*(x2 - x1);
     float sq2 = (y2 - y1)*(y2 - y1);
-    float dist = sqrt(sq1-sq2);
+    float dist = sqrt(sq1+sq2);
     return dist;
 }
 void output(float x1, float y1, float x2, float y2, float distance){
