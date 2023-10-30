403e9b85e99949cec50b12098fb9b071af9d1b5e
abhishek nair <abhisheknair@gmail.com>
2023-10-30 10:01:07 +0530

    committed 10/30/2023, 10:01:06 AM

--- set01/problem09.c
+++ set01/problem09.c
@@ -5,7 +5,7 @@ int main(){
     float i,n;
     printf("n: "); 
     scanf("%f", &n);
-    for(i=0.01;i*i<n;i+=0.001);
-    printf("%.1f",i);
+    for(i=0.01;i*i<n;i+=0.0001);
+    printf("%.4f",i);
     printf("%f",sqrt(n));
 }
