ff3856492565b136ab1893686818a1a2f76d68dc
abhishek nair <abhisheknair@gmail.com>
2023-10-30 10:22:07 +0530

    committed 10/30/2023, 10:22:06 AM

--- set03/problem01.c
+++ set03/problem01.c
@@ -7,7 +7,7 @@ void output(float x1, float y1, float x2, float y2, float distance);
 
 int main(){
     float x1,x2,y1,y2;
-    input(x1,y1,x2,y2);
+    input(&x1,&y1,&x2,&y2);
     float distance = find_distance(x1,y1,x2,y2);
     output(x1,y1,x2,y2,distance);
 }
