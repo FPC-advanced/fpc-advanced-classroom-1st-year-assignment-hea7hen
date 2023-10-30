f5bcba1308bd54eb64536060bbbec1e143a3a9bd
abhishek nair <abhisheknair@gmail.com>
2023-10-30 09:59:37 +0530

    committed 10/30/2023, 9:59:36 AM

--- set01/problem09.c
+++ set01/problem09.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-
+#include <math.h>
 
 int main(){
     float i,n;
     printf("n: "); 
     scanf("%f", &n);
     for(i=0.01;i*i<n;i+=0.01);
-    printf("%.1f",i);
+    printf("%.2f",i);
+    printf("%f",ma)
 }
