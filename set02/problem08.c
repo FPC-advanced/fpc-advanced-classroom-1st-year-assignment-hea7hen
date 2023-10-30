c68afca293d9fae4af1bf920be16e0a397c682ae
abhishek nair <abhisheknair@gmail.com>
2023-10-30 09:53:07 +0530

    committed 10/30/2023, 9:53:06 AM

---
+++ set02/problem08.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+typedef struct _triangle {
+	float base, altitude, area;
+} Triangle;
+int input_n();
+
+Triangle input_triangle();
+void input_n_triangles(int n, Triangle t[n]);
+void find_area(Triangle *t);
+void find_n_areas(int n, Triangle t[n]);
+Triangle find_smallest_triangle(int n, Triangle t[n]);
+void output(int n, Triangle t[n], Triangle smallest);
+
+int main(){
+    int n = input_n;
+    Triangle t;
+}
+int input_n(){
+    int x;
+    printf("Enter the numbers of triangles to be compared with: ");
+    scanf("%d",&x);
+    return x;
+}
+
+void input_n_triangles(int n, Triangle t[n]){
+    for(int i=1;i<=n;i++){
+        printf("Enter triangle %d",i);
+        scanf("%d",t[0])
+    }
+}
---
+++ set03/problem01.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void input(float *x1, float *y1, float *x2, float *y2);
+float find_distance(float x1, float y1, float x2, float y2);
+void output(float x1, float y1, float x2, float y2, float distance);
+
+int main(){
+    float x1,x2,y1,y2;
+    input(&x1,&y1,&x2,&y2);
+    float distance = find_distance(x1,y1,x2,y2);
+    output(x1,y1,x2,y2,distance);
+}
+void input(float *x1, float *y1, float *x2, float *y2){
+    printf("x1: ");
+    scanf("%f",x1);
+    printf("y1: ");
+    scanf("%f",y1);
+    printf("x2: ");
+    scanf("%f",x2);
+    printf("y2: ");
+    scanf("%f",y2);
+}
+void output(float x1, float y1, float x2, float y2, float distance);
