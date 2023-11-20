c86c724d1f9072538d3b986ae9c16163c83852e4
abhishek nair <abhisheknair@gmail.com>
2023-11-20 09:35:54 +0530

    committed 11/20/2023, 9:35:54 AM

--- set01/problem01.c
+++ set01/problem01.c
@@ -1,38 +1,6 @@
 #include <stdio.h>
-#include <string.h>
-void input_string(char* a, char* b){
-  printf("pls enter a str: ");
-  scanf("%s",a);
-  printf("pls nter a str: ");
-  scanf("%s",b);
-}
-
-int sub_str_index(char* string, char* substring){
-  int lens=strlen(substring);
-  int sum;
-  for(int i=0;string[i]!='\0';i++)
-    for(int j=0;substring[j]!='\0';j++)
-      if(string[i]==substring[j]){
-      sum+=1;
-      }
-  if(sum==lens){
-    return 0;
-  }
-  return 1;
-  }
-
-void output(int index){
-  if(index==0){
-    printf("substring is found in string");
-  }
-  else{
-    printf("substring isnt found within string");
-  }
-}
 
 int main(){
-char a[100],b[100];
-  input_string(a,b);
-  int index=sub_str_index(a,b);
-  output(index);
+  printf("Abhishek Nair");
+  return 0;
 }
